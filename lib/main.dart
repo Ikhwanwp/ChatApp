64809c492305463a6c79535bf0c94a78ac28a8de
Ikhwanwp <wiratama900@gmail.com>
2021-12-30 23:14:15 +0700

    .

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:chatapp2/app/utils/loading_page.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(LoadingPage());
+}
+// void main() {
+//   runApp(
+//     GetMaterialApp(
+//       title: "Application",
+//       initialRoute: AppPages.INITIAL,
+//       getPages: AppPages.routes,
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       // home: ,
+//     );
+//   }
+// }
